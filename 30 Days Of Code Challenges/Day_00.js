34d34a99d7b17963a19f9a042eb34d9ba054b9a4
Ozobia Nwadibia <Ozobia.cn3@gmail.com>
2019-07-29 09:55:48 -0500

    Solve Day 00 challenge

---
+++ 30 Days Of Code Challenges/Day_00.js
@@ -0,0 +1,16 @@
+// print two strings to the console or terminal
+
+
+function processData(inputString) {
+  // This line of code prints the first line of output
+  console.log("Hello World.");
+  // Write the second line of output that prints the contents of 'inputString' here.
+  console.log(inputString);
+ 
+}
+
+processData('Welcome to 30 Days of Code!');
+console.log();
+processData('I want to master NodeJS!');
+
+
